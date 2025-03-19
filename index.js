e24accd60219738b3dcb6cadf3f65e5c6c3a9665
ZolWebBuilder <jilaphonschoolmail@gmail.com>
2025-03-19 20:26:40 +0700

    Update index.js

--- index.js
+++ index.js
@@ -4,12 +4,12 @@ const v1 = require('./commands');
 
 const v2 = new Client({ intents: [GatewayIntentBits.Guilds] });
 const v3 = [
-    { name: 'coinflip', description: 'Bet on heads or tails', options: [{ name: 'choice', type: 3, description: 'Heads or Tails', required: true, choices: [{ name: 'Heads', value: 'heads' }, { name: 'Tails', value: 'tails' }] }], integration_types: 0, context: [0, 1, 2] },
-    { name: 'roll', description: 'Roll a dice and guess the number', options: [{ name: 'sides', type: 4, description: 'Number of sides on the dice', required: true }, { name: 'guess', type: 4, description: 'Your guessed number', required: true }], integration_types: 0, context: [0, 1, 2] },
-    { name: 'higherlower', description: 'Guess if the next number is higher or lower', options: [{ name: 'guess', type: 3, description: 'Higher or Lower', required: true, choices: [{ name: 'Higher', value: 'higher' }, { name: 'Lower', value: 'lower' }] }], integration_types: 0, context: [0, 1, 2] },
-    { name: 'rps', description: 'Play Rock, Paper, Scissors', options: [{ name: 'choice', type: 3, description: 'Rock, Paper, or Scissors', required: true, choices: [{ name: 'Rock', value: 'rock' }, { name: 'Paper', value: 'paper' }, { name: 'Scissors', value: 'scissors' }] }], integration_types: 0, context: [0, 1, 2] },
-    { name: 'evenodd', description: 'Guess if a number is even or odd', options: [{ name: 'guess', type: 3, description: 'Even or Odd', required: true, choices: [{ name: 'Even', value: 'even' }, { name: 'Odd', value: 'odd' }] }], integration_types: 0, context: [0, 1, 2] },
-    { name: 'carddraw', description: 'Pick a number from 1-13 and see if you match', options: [{ name: 'number', type: 4, description: 'Pick a number from 1-13', required: true }], integration_types: 0, context: [0, 1, 2] }
+  {name:'coinflip',description:'Bet on heads or tails',options:[{type:3,name:'choice',description:'Heads or Tails',required:true,choices:[{name:'Heads',value:'heads'},{name:'Tails',value:'tails'}]}],integration_types:[0,1],contexts:[0,1,2]},
+  {name:'roll',description:'Roll a dice and guess the number',options:[{type:4,name:'sides',description:'Number of sides on the dice',required:true},{type:4,name:'guess',description:'Your guessed number',required:true}],integration_types:[0,1],contexts:[0,1,2]},
+  {name:'higherlower',description:'Guess if the next number is higher or lower',options:[{type:3,name:'guess',description:'Higher or Lower',required:true,choices:[{name:'Higher',value:'higher'},{name:'Lower',value:'lower'}]}],integration_types:[0,1],contexts:[0,1,2]},
+  {name:'rps',description:'Play Rock, Paper, Scissors',options:[{type:3,name:'choice',description:'Rock, Paper, or Scissors',required:true,choices:[{name:'Rock',value:'rock'},{name:'Paper',value:'paper'},{name:'Scissors',value:'scissors'}]}],integration_types:[0,1],contexts:[0,1,2]},
+  {name:'evenodd',description:'Guess if a number is even or odd',options:[{type:3,name:'guess',description:'Even or Odd',required:true,choices:[{name:'Even',value:'even'},{name:'Odd',value:'odd'}]}],integration_types:[0,1],contexts:[0,1,2]},
+  {name:'carddraw',description:'Pick a number from 1-13 and see if you match',options:[{type:4,name:'number',description:'Pick a number from 1-13',required:true}],integration_types:[0,1],contexts:[0,1,2]}
 ];
 
 v2.once('ready', async () => {
